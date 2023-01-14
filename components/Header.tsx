772733746360ef0e7e309906e9db6a5a2cd096a0
eskeemos <l.michalik004@gmail.com>
2023-01-14 22:54:53 +0100

    project startup

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <Image src='' alt='' />
+    </header>
+  )
+}
+
+export default Header
